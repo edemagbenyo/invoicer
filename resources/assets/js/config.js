96ddec1c3af72156599be9ac33906153473efd7b
Edem Agbenyo <edemagbenyo@gmail.com>
2020-09-01 17:14:35 +0000

    update dev host to localhost

--- resources/assets/js/config.js
+++ resources/assets/js/config.js
@@ -4,7 +4,7 @@ let api_url='';
 
 switch (process.env.NODE_ENV) {
     case 'development':
-        api_url='http://invoice.test/api'
+        api_url='http://localhost:8000/api'
         break;
     case 'production':
         api_url='http://invoice.edemagbenyo.com/api'
